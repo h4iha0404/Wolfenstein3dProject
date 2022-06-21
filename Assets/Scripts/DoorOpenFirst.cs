ee8ab3564e839182a65a3801c2c980adb8fd5785
h4iha <h4iha2000@gmail.com>
2022-06-21 22:32:22 +0700

    21/6

---
+++ Assets/Scripts/DoorOpenFirst.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenFirst : MonoBehaviour
+{
+    public GameObject theDoor;
+    private void OnTriggerEnter(Collider other) {
+        theDoor.GetComponent<Animator>().Play("OpenDoor");
+    }
+}
