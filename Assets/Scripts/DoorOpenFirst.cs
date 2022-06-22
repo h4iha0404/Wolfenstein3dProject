154973791955a3f863154366042b910da4dc1ed3
h4iha <h4iha2000@gmail.com>
2022-06-23 05:54:27 +0700

    23/6

--- Assets/Scripts/DoorOpenFirst.cs
+++ Assets/Scripts/DoorOpenFirst.cs
@@ -5,7 +5,19 @@ using UnityEngine;
 public class DoorOpenFirst : MonoBehaviour
 {
     public GameObject theDoor;
+    public AudioSource doorFX;
+
     private void OnTriggerEnter(Collider other) {
-        theDoor.GetComponent<Animator>().Play("OpenDoor");
+        doorFX.Play();
+        theDoor.GetComponent<Animator>().Play("DoorOpen");
+        this.GetComponent<CapsuleCollider>().enabled = false;
+        StartCoroutine(CloseDoor());
+    }
+
+    IEnumerator CloseDoor() {
+        yield return new WaitForSeconds(5);
+        doorFX.Play();
+        theDoor.GetComponent<Animator>().Play("DoorClose");
+        this.GetComponent<CapsuleCollider>().enabled = true;
     }
 }
