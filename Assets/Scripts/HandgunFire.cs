1a9fda77f77f1be86a41f3fe015d6630be70f381
h4iha <h4iha2000@gmail.com>
2022-06-27 09:55:26 +0700

    1

--- Assets/Scripts/HandgunFire.cs
+++ Assets/Scripts/HandgunFire.cs
@@ -7,27 +7,34 @@ public class HandgunFire : MonoBehaviour
     public GameObject theGun;
     public GameObject muzzleFlash;
     public AudioSource gunFire;
+    public AudioSource emptyAmmoSound;
     public bool isFiring = false;
     
     void Update()
     {
         if (Input.GetButtonDown("Fire1")) {
-            if (isFiring == false) {
-                StartCoroutine(FiringHandgun());
+            if (GlobalAmmo.handgunAmmo < 1) {
+                emptyAmmoSound.Play();
             }
 
+            else {
+                if (isFiring == false) {
+                    StartCoroutine(FiringHandgun());
+                }  
+            }
         }
     }
 
     IEnumerator FiringHandgun() {
         isFiring = true;
-        //theGun.GetComponent<Animator>().Play("HandgunFire");
+        GlobalAmmo.handgunAmmo -= 1;
+        theGun.GetComponent<Animator>().Play("HandgunFire");
         muzzleFlash.SetActive(true);
         gunFire.Play();
         yield return new WaitForSeconds(0.05f);
         muzzleFlash.SetActive(false);
-        yield return new WaitForSeconds(0.45f);
-        //theGun.GetComponent<Animator>().Play("New State");
+        yield return new WaitForSeconds(0.25f);
+        theGun.GetComponent<Animator>().Play("New State");
         isFiring = false;
     }
 }
